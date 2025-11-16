4f3a5b450782d0a0a4e3e3bdfed44df577d51e0e
BernardoBasaldua <bernardobasaldua@gmail.com>
2025-11-16 02:21:47 -0300

    Se ordenó el código en general y se agregó la funcionalidad de reducción de stock disponible

--- m0_compras.py
+++ m0_compras.py
@@ -5,6 +5,8 @@ from m2_verificacion_disponibilidad import Deposito
 
 from modelos.pedido import Producto
 
+seleccionados = []
+
 def cargar_productos_desde_json(deposito):
     data = deposito.cargar_stock()
     # with open(archivo, "r", encoding="utf-8") as f:
@@ -25,18 +27,15 @@ def cargar_productos_desde_json(deposito):
 
     return productos
 
-def seleccionar_productos(deposito):
-    
-    print("++++BIENVENIDO AL SISTEMA DE COMPRAS ONLINE++++")
-    
+
+def obtener_cliente():
     #  Datos del cliente
     while True:
-        productos_disponibles = cargar_productos_desde_json(deposito)
         cliente = input(" Ingrese su nombre: ").strip()
         if cliente.strip():
             break
         else: 
-             print(" Nombre inválido. Intente nuevamente.")
+             print(" ERROR: Nombre invalido\n")
     
     while True:
         email = input(" Ingrese su email: ").strip()
@@ -44,72 +43,92 @@ def seleccionar_productos(deposito):
         if "@" in email and "." in email.split("@")[-1]:
             break
         else:
-            print(" Email inválido. Intente nuevamente.")
+            print(" ERROR: Email invalido\n")
 
     while True:
         direccion = input(" Ingrese su dirección de envío: ").strip()
         if direccion.strip():
             break
         else:
-            print(" Direccion inválido. Intente nuevamente.")
+            print(" ERROR: Direccion invalido\n")
+    
+    return {
+        "cliente": cliente,
+        "email": email,
+        "direccion": direccion
+    }
 
+
+def seleccionar_productos(deposito):
+    
     #Selección de productos
-    seleccionados = []
+    productos_disponibles = cargar_productos_desde_json(deposito)
     while True:
-        print("\n Lista de productos disponibles:")
+        print("\n ----------- Productos disponibles -----------\n")
         for i, producto in enumerate(productos_disponibles):
             print(f"{i}. {producto}")
 
+        print()
         while True:
             try:
                 indice = int(input(" Ingrese el número del producto que desea agregar: "))
                 if (0 <= indice < len(productos_disponibles) ):
                     break   # si llega hasta acá → es válido → rompemos el bucle
                 else:
-                    print("Índice inválido.")
+                    print(" ERROR: Indice invalido\n")
             except ValueError:
-                print("Valor inválido. Intente nuevamente.")
+                print(" ERROR: Valor invalido\n")
         
         while True:
             try:
                 cantidad = int(input(" Ingrese cantidad del producto que desea comprar: "))
-                if (cantidad >= 0 ):
+                if (0 < cantidad <= productos_disponibles[indice].stock): # MODIFIQUE PARA SEA SOLO MAYOR A CERO, NO TIENE SENTIDO QUE SEA IGUAL A CERO. LIMITÉ A CANTIDAD EXISTENTE
                     break   # si llega hasta acá → es válido → rompemos el bucle
-                else:
-                    print(" La cantidad debe ser mayor o igual a cero.")
+                elif (cantidad == 0): print(" ERROR: Indique una cantidad mayor a cero\n")
+                else: print(" ERROR: No hay stock disponible\n")
             except ValueError:
-                print("Valor inválido. Intente nuevamente.")
+                print(" ERROR: Valor invalido\n")
 
         productos_disponibles[indice].cantidad_solicitada = cantidad
         seleccionados.append(productos_disponibles[indice])
-        print(f" Agregado: {productos_disponibles[indice].nombre} Cantidad:{cantidad}")
+        print(f"---Agregado: {productos_disponibles[indice].nombre}---\n---Cantidad:{cantidad}---")
+        productos_disponibles[indice].stock = productos_disponibles[indice].stock - cantidad # ESTA LINEA ES LA QUE PERMITE MODIFICAR STOCK DISPONIBLE
 
+        print()
         continuar = input("¿Desea agregar otro producto? (s/n): ").strip().lower()
         #Aca el profe ingreso x, y se procedió a la forma de pago.
         while (continuar != "n" and continuar != "s"): 
-            print("Ingrese (s/n)")
+            print(" ERROR: Opcion no valida - Ingrese (s/n)\n")
             continuar = input("¿Desea agregar otro producto? (s/n): ").strip().lower()
             #LLAMAR A UNA FUNCION DE RESERVA
         if continuar == "n":
             break
 
+
+def definir_pago():
     #  Forma de pago (selección por número)
     total = sum(p.precio * p.cantidad_solicitada for p in seleccionados)
     forma_de_pago = ""
     while forma_de_pago == "":
-        print(f"\n Total a pagar: {total}\n Formas de pago disponibles:")
+        print(f"\n----- Total a pagar: {total} -----\n Formas de pago disponibles:")
         print("1. Transferencia")
         print("2. Tarjeta")
-        opcion = input("Ingrese el número de la forma de pago (1 o 2): ").strip()
+        opcion = input("Ingrese el número de la forma de pago ( 1 / 2 ): ").strip()
         while (opcion != "1" and opcion != "2"): 
-            print("Ingrese (1 o 2)")
-            opcion = input("Ingrese el número de la forma de pago (1 o 2): ").strip()
+            print(" ERROR: Opcion no valida\n")
+            opcion = input("Ingrese el número de la forma de pago ( 1 / 2 ): ").strip()
         if opcion == "1":
             forma_de_pago = "transferencia"
         elif opcion == "2":
             forma_de_pago = "tarjeta"
 
+    return {
+        "total": total,
+        "forma_de_pago": forma_de_pago
+    }
 
+
+def definir_envio():
     #  Forma de envío (selección por número)
     metodo_envio = ""
     while metodo_envio == "":
@@ -117,23 +136,30 @@ def seleccionar_productos(deposito):
         print("1. Estándar")
         print("2. Express")
         print("3. Pickup")
-        opcion = input("Ingrese el número del método de envío (1, 2 o 3): ").strip()
+        opcion = input("Ingrese el número del método de envío ( 1 / 2 / 3 ): ").strip()
         while (opcion != "1" and opcion != "2" and opcion != "3"): 
-            print("Ingrese (1, 2 o 3)")
-            opcion = input("Ingrese el número del método de envío (1, 2 o 3): ").strip()
+            print(" ERROR: Opcion no valida\n")
+            opcion = input("Ingrese el número del método de envío ( 1 / 2 / 3 ): ").strip()
         if opcion == "1":
             metodo_envio = "estandar"
         elif opcion == "2":
             metodo_envio = "express"
         elif opcion == "3":
             metodo_envio = "pickup"
+    
+    return metodo_envio
 
 
+def armar_pedido(deposito): #TODAS LAS FUNCIONES SE EJECUTAN DESDE ESTA FUNCION
     #  Armar datos finales
+    datos = obtener_cliente()
+    seleccionar_productos(deposito)
+    pago = definir_pago()
+    envio = definir_envio()
     pedido = {
-        "cliente": cliente,
-        "email": email,
-        "direccion_envio": direccion,
+        "cliente": datos["cliente"],
+        "email": datos["email"],
+        "direccion_envio": datos["direccion"],
         "productos": [
             {
                 "sku": p.sku,
@@ -143,11 +169,11 @@ def seleccionar_productos(deposito):
             } for p in seleccionados
         ],
         "datos_del_pago": {
-            "metodo": forma_de_pago,
-            "total_abonado":total,
-            "numero_operacion":random.randint(1000, 20000)
+            "metodo": pago["forma_de_pago"],
+            "total_abonado": pago["total"],
+            "numero_operacion": random.randint(1000, 20000)
         },
-        "metodo_envio" : metodo_envio
+        "metodo_envio" : envio
     }
 
     return pedido
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from m0_compras import seleccionar_productos 
+from m0_compras import armar_pedido, obtener_cliente, seleccionar_productos 
 from m1_recepcion_del_pedido import RecepcionPedido
 from m2_verificacion_disponibilidad import Verificacion_disponibilidad_producto, Deposito
 from m3_procesamiento_de_pago import ModuloPago, Api_banco
@@ -12,81 +12,83 @@ from modulo_notificaciones import Modulo_Notificaciones
 stock = "stock.json"
 deposito = Deposito()  
 
+modulo_Notificaciones = Modulo_Notificaciones()
+print("\n++++++++++++++++++ BIENVENIDO AL SISTEMA DE COMPRAS ONLINE ++++++++++++++++++\n")
+
+##EL CLIENTE SELECCIONA EL PEDIDO
+pedido_source = armar_pedido(deposito)
+recepcion_pedido = RecepcionPedido(pedido_source)
+
 if __name__ == "__main__":
-    
-    ##EL CLIENTE SELECCIONA EL PEDIDO
-    pedido_source = seleccionar_productos(deposito)
 
     #####VALIDAMOS DATOS PEDIDO MODULO1########
-    recepcion_pedido = RecepcionPedido(pedido_source)
     validacion_datos_pedido = recepcion_pedido.validar_pedido()
     #CREAMOS PEDIDO 
-    modulo_Notificaciones = Modulo_Notificaciones()
     if validacion_datos_pedido:
-        print (f"el pedido fue validado {validacion_datos_pedido}")##MODULO NOTIFICACIONES
+        print (f"Validez del pedido: {validacion_datos_pedido}")##MODULO NOTIFICACIONES
         orden_pedido = recepcion_pedido.crear_pedido()
-        print (f"el pedido fue CREADO CON EXITO {orden_pedido}")
+        print (f"\n--------- Pedido CREADO CON EXITO ---------\n")
+        print(orden_pedido)
+        print (f"-------------------------------------------\n")
 
         ######VERIFICAMOS DISPONIBILIDAD MODULO 2#############   
         # deposito = Deposito()  
         verificacion_disponibilidad = Verificacion_disponibilidad_producto(orden_pedido, deposito)
         disponibilidad_stock = verificacion_disponibilidad.consultar_stock()
-        print (f"disponibilidad productos: {disponibilidad_stock}")
+        print (f"Disponibilidad de productos: {disponibilidad_stock}")
 
         if disponibilidad_stock:
             #CREAMOS LA RESERVA
             reserva = verificacion_disponibilidad.reservar()
             stock_reservado = verificacion_disponibilidad.reservas
-            print("STOCK RESERVADO: \n")
+
+            print("\n--------- STOCK RESERVADO ---------\n")
+            i=1
             for producto in verificacion_disponibilidad.stock_reservado:
-                print(f"""
-                Producto reservado:
-                ▸ Nombre: {producto['nombre']}
-                ▸ SKU: {producto['sku']}
-                ▸ Cantidad reservada: {producto['cantidad_solicitada']}
-                """)
+                print(f"Producto {i}:\n▸ Nombre: {producto['nombre']}\n▸ SKU: {producto['sku']}\n▸ Cantidad reservada: {producto['cantidad_solicitada']}\n")
+                i += 1
                 #▸ Expiración: {producto['expiracion'].strftime('%Y-%m-%d %H:%M:%S')}
                 #######VERIFICACION DE PAGO MODULO 3########
                 api_banco=Api_banco
                 moduloPago = ModuloPago(orden_pedido, api_banco)
                 aprobacion_pago = moduloPago.verificar_pago()
                 
-                if aprobacion_pago:
-                    #NOTIFICAR ACEPTAR PEDIDO
-                    modulo_Notificaciones.notificar("PAGO APROBADO")
+            if aprobacion_pago:
+                #NOTIFICAR ACEPTAR PEDIDO
+                modulo_Notificaciones.notificar("\n--------- PAGO APROBADO ---------\n")
 
-                    ######PREPARACION DEL PEDIDO MODULO 4############# 
-                    moduloPreparacion = ModuloPreparacion() 
-                    preparacion_pedido = moduloPreparacion.preparar_pedido(orden_pedido)
+                ######PREPARACION DEL PEDIDO MODULO 4############# 
+                moduloPreparacion = ModuloPreparacion() 
+                preparacion_pedido = moduloPreparacion.preparar_pedido(orden_pedido)
 
-                    if preparacion_pedido:
-                        print("PEDIDO PREPARADO")
+                if preparacion_pedido:
+                    print("\n--------- PEDIDO PREPARADO ---------\n")
 
-                        ######ENVIO Y SEGUIMIENTO MODULO 5############# 
-                        moduloEnvios = ModuloEnvios()
-                        envio = moduloEnvios.procesar_envio(orden_pedido)
-                        #print (f"el pedido fue ENVIADO CON EXITO {orden_pedido}")
-                        modulo_Notificaciones.enviar_seguimiento(envio.tracking_id, envio.carrier)
+                    ######ENVIO Y SEGUIMIENTO MODULO 5############# 
+                    moduloEnvios = ModuloEnvios()
+                    envio = moduloEnvios.procesar_envio(orden_pedido)
+                    #print (f"el pedido fue ENVIADO CON EXITO {orden_pedido}")
+                    modulo_Notificaciones.enviar_seguimiento(envio.tracking_id, envio.carrier)
 
-                    else: #MODULO 4
-                        print("PEDIDO RECHAZADO")
-                        modulo_Notificaciones.pedido_rechazado(moduloPreparacion.errores)
+                else: #MODULO 4
+                    print("\n--------- PEDIDO RECHAZADO ---------\n")
+                    modulo_Notificaciones.pedido_rechazado(moduloPreparacion.errores)
 
-                else:#MODULO 3
-                    #NOTIFICAR RECHAZAR PROBLEMA DE PAGO
-                    #print("rechazo por problemas con el pago")
-                    modulo_Notificaciones.pedido_rechazado(moduloPago.errores)
-        else:#MODULO 2
-            #print("stock insuficiente")
-            #NOTIFICAR POR FALTA DE STOCK
-            modulo_Notificaciones.pedido_rechazado(["No hay stock suficiente"])
+            else:#MODULO 3
+                #NOTIFICAR RECHAZAR PROBLEMA DE PAGO
+                #print("rechazo por problemas con el pago")
+                modulo_Notificaciones.pedido_rechazado(moduloPago.errores)
+    else:#MODULO 2
+        #print("stock insuficiente")
+        #NOTIFICAR POR FALTA DE STOCK
+        modulo_Notificaciones.pedido_rechazado(["No hay stock suficiente"])
     
-    else:#MODULO1
-        # for error in recepcion_pedido.errores_pedido():
-        #     print (error)
-        #NOTIFICAR FALTA DE DATOS EN LA SOLICITUD DE PEDIDO
-        modulo_Notificaciones.pedido_rechazado(recepcion_pedido.errores)
-        #FALTA PERSISTIR DESDE EL MODULO RECEPCION Y NOTIFICAR
+else:#MODULO1
+    # for error in recepcion_pedido.errores_pedido():
+    #     print (error)
+    #NOTIFICAR FALTA DE DATOS EN LA SOLICITUD DE PEDIDO
+    modulo_Notificaciones.pedido_rechazado(recepcion_pedido.errores)
+    #FALTA PERSISTIR DESDE EL MODULO RECEPCION Y NOTIFICAR
 
 
 
